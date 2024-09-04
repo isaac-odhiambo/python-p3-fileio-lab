e13f3503c057cb0f2030c7ece9f56dde29b5d6a7
Isaac Odiwuor Odhiambo <odhiamboisaach@gmail.com>
2024-09-04 20:22:46 +0300

    done

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,14 @@
 def write_file(file_name, file_content):
-    pass
+    
+    with open(f'{file_name}.txt', 'w', encoding='utf-8') as file:
+        file.write(file_content)
 
-def append_file(file_name, append_content):
-    pass
+def append_file(file_name, file_content):
+    
+    with open(f'{file_name}.txt', 'a', encoding='utf-8') as file:
+        file.write(file_content)
 
 def read_file(file_name):
-    pass
+    
+    with open(f'{file_name}.txt', 'r', encoding='utf-8') as file:
+        return file.read()
